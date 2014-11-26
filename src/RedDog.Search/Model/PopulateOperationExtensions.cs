559c9cbd3c4450157249e7d99d4913e58f22ec0c
sandrinodimattia <contact@sandrino.be>
2014-11-26 18:59:58 +0100

    Release 0.5.2.0
    Support for analyzers, helper for GeoPoint field.

--- src/RedDog.Search/Model/PopulateOperationExtensions.cs
+++ src/RedDog.Search/Model/PopulateOperationExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace RedDog.Search.Model
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace RedDog.Search.Model
 {
     public static class PopulateOperationExtensions
     {
@@ -6,6 +10,35 @@
         {
             operation.Properties.Add(name, value);
             return operation;
+        }
+
+        public static IndexOperation WithGeographyPoint(this IndexOperation operation, string name, double longitude, double latitude)
+        {
+            operation.Properties.Add(name, new GeoPointField(longitude, latitude));
+            return operation;
+        }
+
+    }
+    internal class GeoPointField
+    {
+        [JsonProperty(PropertyName =  "type")]
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty(PropertyName = "coordinates")]
+        public Double[] Coordinates
+        {
+            get;
+            set;
+        }
+
+        public GeoPointField(params double[] coordinates)
+        {
+            Type = "Point";
+            Coordinates = coordinates;
         }
     }
 }
