321939025ab4d8366f13a22d3be1a4d56e85f648
PulsarBlow <pulsarblow@gmail.com>
2014-12-04 12:19:32 +0100

    Fixes #9 - Added PopulateBatchAsync method to IndexManagementClient class. This method splits the IndexOperation param array into parts matching the REST API batch limit. - Added unit test for the List<T>.Chunk() extension method.

---
+++ src/RedDog.Search.Tests/Extensions/ListExtensionsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace RedDog.Search.Tests
+{
+    [TestClass]
+    public class ListExtensionsTest
+    {
+        [TestMethod]
+        public void ChunkTest()
+        {
+            List<int> list = CreateList(999);
+            List<List<int>> chunked = list.Chunk(1000);
+            Assert.IsNotNull(chunked);
+            Assert.IsTrue(chunked.Count == 1);
+            Assert.IsTrue(chunked[0].Count == 999);
+
+
+            list = CreateList(1000);
+            chunked = list.Chunk(1000);
+            Assert.IsNotNull(chunked);
+            Assert.IsTrue(chunked.Count == 1);
+            Assert.IsTrue(chunked[0].Count == 1000);
+
+            list = CreateList(1001);
+            chunked = list.Chunk(1000);
+            Assert.IsNotNull(chunked);
+            Assert.IsTrue(chunked.Count == 2);
+            Assert.IsTrue(chunked[0].Count == 1000);
+            Assert.IsTrue(chunked[1].Count == 1);
+        }
+
+        private List<int> CreateList(int elementCount)
+        {
+            if (elementCount <= 0) { return new List<int>(); }
+            List<int> result = new List<int>();
+            for (int i = 0; i < elementCount; i++)
+            {
+                result.Add(i);
+            }
+            return result;
+        }
+    }
+}
